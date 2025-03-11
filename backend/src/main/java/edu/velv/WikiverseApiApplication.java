01cb478ab1a59cd6c406cd270e2e668f9c6281b4
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-03-10 20:01:49 -0600

    commit sprint initlzr output for webapp w/ needed dependencies, commits maven app

---
+++ backend/src/main/java/edu/velv/WikiverseApiApplication.java
@@ -0,0 +1,13 @@
+package edu.velv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiverseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiverseApiApplication.class, args);
+	}
+
+}
