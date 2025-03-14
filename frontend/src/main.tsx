935a0e7805b8db413ce6337ee7e6ce2db5c3bf31
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-03-13 23:39:15 -0600

    move to declutter main files, update main to reimport

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
-import './index.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Wikiverse } from './app/wikiverse'
+import "./assets/styles/index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Wikiverse } from "./app/wikiverse";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Wikiverse />
-  </StrictMode>)
+  </StrictMode>
+);
