c6e6f6c9347c565886ef71378c99ade0c5ab7599
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-03-18 23:15:13 -0600

    reflects addition of property from backend work

--- frontend/src/types/core/interfaces/vertex.ts
+++ frontend/src/types/core/interfaces/vertex.ts
@@ -1,31 +1,36 @@
-import { Point3D } from './point-3D';
+import { Point3D } from "./point-3D";
 
 /**
  * A single topic or node inside of the {@link Graphset}.
  */
 export interface Vertex {
-	/**
-	 * Unique identifier for the vertex.
-	 */
-	id: string;
+  /**
+   * Unique identifier for the vertex.
+   */
+  id: string;
 
-	/**
-	 * Label or name of the vertex.
-	 */
-	label: string;
+  /**
+   * Label or name of the vertex.
+   */
+  label: string;
 
-	/**
-	 * Description providing additional details about the vertex.
-	 */
-	description: string;
+  /**
+   * Description providing additional details about the vertex.
+   */
+  description: string;
 
-	/**
-	 * Position of the vertex in 3D space.
-	 */
-	position: Point3D;
+  /**
+   * Position of the vertex in 3D space.
+   */
+  position: Point3D;
 
-	/**
-	 * Indicates whether the vertex is locked and cannot be moved.
-	 */
-	locked: boolean;
+  /**
+   * Indicates whether the vertex is locked and cannot be moved.
+   */
+  locked: boolean;
+
+  /**
+   * Weather of not the statements have been collected on this particular piece of data (indicating it is pre-fetch from Wikidata to get the full EntityDocument results)
+   */
+  fetchedEdges: boolean;
 }
