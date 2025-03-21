c6ed70c857dfbd3fc158482d3b88c416d318f2d4
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-03-20 22:54:33 -0600

    move to iconed folder for easy picking out

--- frontend/src/components/main/main/main-display.tsx
+++ frontend/src/components/main/main/main-display.tsx
@@ -1,6 +1,7 @@
 import "./main-display.css";
 import { ParticlesSketch } from "../particles-sketch/particles-sketch";
-import { LandingPage } from "../landing-page/landing-page";
+import { LandingPage } from "../landing-page";
+
 
 /**
  * Primary Component in Grid position 5 - contains a majority of the details onscreen at a given time.
