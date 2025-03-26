30e0f9f0671c91b3e0c8c90b63189c5215720fbb
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-03-25 23:46:42 -0600

    init misc needed interfaces

---
+++ frontend/src/types/core/sketch/edge-directions.ts
@@ -0,0 +1,8 @@
+/**
+ * Defines the "flow" of an Edge based on a provided context {@link Vertex}
+ */
+export enum EdgeDirections {
+  INCOMING = "incoming",
+  OUTGOING = "outgoing",
+  PARALLEL = "parallel",
+}
