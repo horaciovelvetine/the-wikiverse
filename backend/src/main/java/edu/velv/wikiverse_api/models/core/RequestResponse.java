4bc1c077b4549f3730acef91fdec4c1a59db4742
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-04-08 21:01:41 -0600

    remove addition of wikiverse prefx to match ClientRequest assumption on backend naming scheme

--- backend/src/main/java/edu/velv/wikiverse_api/models/core/RequestResponse.java
+++ backend/src/main/java/edu/velv/wikiverse_api/models/core/RequestResponse.java
@@ -2,23 +2,23 @@ package edu.velv.wikiverse_api.models.core;
 
 import edu.velv.wikiverse_api.models.ClientRequest;
 
-public class WikiverseRequestResponse {
+public class RequestResponse {
   public Graphset data;
   public WikiverseError error;
   public String message;
 
-  public WikiverseRequestResponse(ClientRequest request) {
+  public RequestResponse(ClientRequest request) {
     this.data = request.graph;
     this.error = request.error;
     this.message = "the Wikiverse API is Currently Online.";
   }
 
-  public WikiverseRequestResponse(String statusMessage) {
+  public RequestResponse(String statusMessage) {
     this.data = new Graphset();
     this.message = statusMessage;
   }
 
-  public WikiverseRequestResponse(WikiverseError serviceFaultError) {
+  public RequestResponse(WikiverseError serviceFaultError) {
     this.data = new Graphset();
     this.message = serviceFaultError.message();
     this.error = serviceFaultError;
