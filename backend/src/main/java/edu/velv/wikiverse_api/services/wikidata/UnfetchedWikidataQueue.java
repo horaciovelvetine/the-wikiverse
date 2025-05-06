f717ced13a647e83f25cc255a1308dee41c894cd
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-05-05 20:46:02 -0600

    Update UnfetchedWikidataQueue to use Edge type for unfetched entities. Removed the addValueToQueue method to streamline the class, focusing on managing the list of edges pending retrieval from the Wikidata API.

--- backend/src/main/java/edu/velv/wikiverse_api/services/wikidata/UnfetchedWikidataQueue.java
+++ backend/src/main/java/edu/velv/wikiverse_api/services/wikidata/UnfetchedWikidataQueue.java
@@ -4,27 +4,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import edu.velv.wikiverse_api.models.core.Edge;
+
 /**
  * Used to store Entity details which are yet to be fetched from the WikidataAPI, working primarily in tandem with the {@link WikidataFetchBroker}.
  */
 public class UnfetchedWikidataQueue {
-  private final List<WikidataValue> unfetched = Collections.synchronizedList(new ArrayList<>());
+  private final List<Edge> unfetched = Collections.synchronizedList(new ArrayList<>());
 
   /**
    * @return a boolean if there are any unfetched values in the queue
    */
   public boolean hasUnfetchedData() {
     return this.unfetched.size() != 0 && this.unfetched.size() > 0;
-  }
-
-  /**
-   * Adds a {@link WikidataValue} to the queue and checks to prevent adding duplicates.
-   */
-  public void addValueToQueue(WikidataValue value) {
-    synchronized (unfetched) {
-      if (!unfetched.contains(value)) {
-        unfetched.add(value);
-      }
-    }
   }
 }
