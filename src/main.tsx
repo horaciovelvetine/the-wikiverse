da1ae9c99a161c4f64d8a185d34ac831a2f7f04c
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-10-02 21:38:49 -0600

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import "./assets/styles/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Wikiverse } from "../_v1/app/wikiverse";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Wikiverse />
+    <p>Hello World</p>
   </StrictMode>
 );
