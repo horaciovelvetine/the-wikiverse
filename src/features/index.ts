34d53e733cc1e694c49564b2ea66b975465d65cd
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-10-25 22:14:49 -0600

    Update feature exports and enhance routing: Add APIOfflineNotice and LandingPage components, and refactor Index component to conditionally render based on service status and graphset state.

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,6 +1,8 @@
-export { PageNotFound } from "./page-not-found";
-export { NavbarInput, SiteLogo, NavbarDropDownMenu } from "./navbar";
-export { ParticlesSketch } from "./particles-sketch";
-export { LandingPageCardMain } from "./landing-page";
+export { APIOfflineNotice } from "./api-offline-notice";
 export { Footer } from "./footer";
+export { LandingPage } from "./landing-page";
+export { NavbarInput, SiteLogo, NavbarDropDownMenu } from "./navbar";
+export { PageNotFound } from "./page-not-found";
+export { ParticlesSketch } from "./particles-sketch";
+export { PendingRequestIndicator } from "./pending-request-indicator";
 export { WikiverseSketch } from "./wikiverse-sketch";
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,53 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { LandingPageCardMain, WikiverseSketch } from "../features";
+import { APIOfflineNotice, WikiverseSketch, LandingPage } from "../features";
+import {
+  useCameraSettings,
+  useGraphsetState,
+  useLayoutSettings,
+  useSketchSettings,
+  useWikiverseService,
+} from "../hooks";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  const { serviceOnline } = useWikiverseService();
+  const sketchSettings = useSketchSettings();
+  const cameraSettings = useCameraSettings();
+  const layoutSettings = useLayoutSettings();
+  const graphsetState = useGraphsetState();
+
   return (
-    <div className="h-full flex items-center justify-center">
-      {/* <LandingPageCardMain /> */}
-      <WikiverseSketch />
-    </div>
+    <>
+      <div className="h-full flex items-center justify-center">
+        {/* OFFLINE NOTICE */}
+        {/* {!serviceOnline && <APIOfflineNotice />} */}
+        {/* ====================================================================== */}
+        {/* LANDING PAGE  */}
+        {serviceOnline && !graphsetState.graphset && (
+          <LandingPage
+            prefers3D={layoutSettings.prefers3D}
+            setPrefers3D={layoutSettings.setPrefers3D}
+            setSketchQuery={sketchSettings.setSketchQuery}
+            wikiLangTarget={sketchSettings.wikiLangTarget}
+            setWikiLangTarget={sketchSettings.setWikiLangTarget}
+          />
+        )}
+        {/* ====================================================================== */}
+        {/* WIKIVERSE SKETCH  */}
+        {graphsetState.graphset && (
+          <WikiverseSketch
+            graphsetState={graphsetState}
+            sketchSettings={sketchSettings}
+            cameraSettings={cameraSettings}
+            layoutSettings={layoutSettings}
+          />
+        )}
+        {/* ====================================================================== */}
+      </div>
+    </>
   );
 }
