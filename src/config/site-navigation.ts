d5dcf6279200e4a769eb044e2359164411e05d13
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-10-30 11:56:19 -0600

    Add docs

--- src/config/site-navigation.ts
+++ src/config/site-navigation.ts
@@ -1,5 +1,8 @@
 import { NavigationItem } from "../types";
 
+/**
+ * Defines the Pages managed by Tanstack Router
+ */
 export const SiteNavigation: NavigationItem[] = [
   { id: "home-page", to: "/", text: "Home" },
   { id: "about-page", to: "/about", text: "About" },
