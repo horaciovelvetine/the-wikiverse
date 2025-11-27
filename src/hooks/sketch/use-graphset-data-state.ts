7f07617148fa0de0d39dc7993df22661d368ebfb
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-11-27 04:05:25 -0500

    Enhance graphset data state management: Introduce relatedEdges computed property to track edges connected to the selected vertex, improving data handling and interaction within the graphset context.

--- src/hooks/sketch/use-graphset-data-state.ts
+++ src/hooks/sketch/use-graphset-data-state.ts
@@ -1,14 +1,25 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { GraphsetDataState } from "../../types/sketch";
-import { GraphsetData, VertexData, UserInteraction } from "../../types";
+import { EdgeData, GraphsetData, VertexData } from "../../types";
 
 export function useGraphsetDataState(): GraphsetDataState {
   const [selectedVertex, setSelectedVertex] = useState<VertexData | null>(null);
   const [hoveredVertex, setHoveredVertex] = useState<VertexData | null>(null);
   const [graphset, setGraphset] = useState<GraphsetData | null>(null);
-  const [interactionHistory, setInteractionHistory] = useState<
-    UserInteraction[]
-  >([]);
+
+  /**
+   * An array of EdgeData objects that are connected to the currently selected vertex.
+   * This value updates whenever the selected vertex or the graphset changes.
+   * If no vertex is selected or no graphset loaded, this will be an empty array.
+   */
+  const relatedEdges: EdgeData[] = useMemo(() => {
+    return (
+      graphset?.edges.filter(
+        e =>
+          e.sourceID === selectedVertex?.id || e.targetID === selectedVertex?.id
+      ) || []
+    );
+  }, [selectedVertex, graphset]);
 
   return {
     graphset,
@@ -17,7 +28,6 @@ export function useGraphsetDataState(): GraphsetDataState {
     setSelectedVertex,
     hoveredVertex,
     setHoveredVertex,
-    interactionHistory,
-    setInteractionHistory,
+    relatedEdges,
   };
 }
