90dd2c4e948b95e26d5bd1b8a55db96b1a602ca1
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-12-02 11:09:22 -0700

    Add `useKeyboardShortcutListerner` export in `index.ts`: Include the new custom hook for handling keyboard shortcuts to enhance user interaction capabilities. Update existing exports for consistency.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,9 +3,9 @@ export { useWikiverseService } from "./api/use-wikiverse-service";
 
 //?/==> SITE
 export { useDebouncedValue } from "./site/use-debounced-value";
+export { useKeyboardShortcutListerner } from "./site/use-keyboard-shortcut-listener";
 
 //?/==> SKETCH
 export { useSketchSettingsState } from "./sketch/use-sketch-settings-state";
 export { useLayoutSettingsState } from "./sketch/use-layout-settings-state";
-export { useGraphsetDataState } from "./sketch/use-graphset-data-state";
 export { useCameraSettingsState } from "./sketch/use-camera-settings-state";
