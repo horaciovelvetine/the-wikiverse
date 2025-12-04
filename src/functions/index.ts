7abc08032b2a58305f1df4cd2d3cdcf2d7e3bfa1
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-12-03 23:08:00 -0700

    Add fuzzy matching functionality: Introduce `isFuzzyMatch` function for improved search capabilities, allowing for typo tolerance in text matching. Update `index.ts` to export the new function.

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -3,3 +3,4 @@ export { getWikiverseSketchContainer } from "./get-wikiverse-sketch-container";
 export { isAPIRequestObject } from "./is-api-request-object";
 export { getEdgeDirectionIcon } from "./get-edge-direction-icon";
 export { hasParallelEdgeInSet } from "./has-parallel-edge-in-set";
+export { isFuzzyMatch } from "./is-fuzzy-match";
