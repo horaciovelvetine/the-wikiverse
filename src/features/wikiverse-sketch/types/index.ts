9774e158f39f1aea77bf2cf787c229547c2a108a
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-12-03 23:16:18 -0700

    Add MinMaxSet and MinMaxValues types: Introduce new types for managing minimum and maximum values in a 3D space, enhancing type safety and structure in the wikiverse-sketch feature.

---
+++ src/features/wikiverse-sketch/types/index.ts
@@ -0,0 +1,2 @@
+export type { MinMaxSet } from "./min-max-set";
+export type { MinMaxValues } from "./min-max-values";
---
+++ src/features/wikiverse-sketch/types/min-max-set.ts
@@ -0,0 +1,7 @@
+import { MinMaxValues } from "./min-max-values";
+
+export interface MinMaxSet {
+  x: MinMaxValues;
+  y: MinMaxValues;
+  z: MinMaxValues;
+}
